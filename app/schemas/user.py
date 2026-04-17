39ad198581cfe3477b45da992a9abd872d757709
Firdawsbenahmed <firdawsbenahmed20@gmail.com>
2026-04-17 11:48:19 +0100

    user_schema

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel , EmailStr
+
+class UserCreate(BaseModel): 
+    name : str 
+    email: EmailStr
+    password : str
+    company_id : int
+    role : str = "Admin"
+
+class LoginUser(BaseModel):
+    email : EmailStr
+    password : str 
+
+class UserResponse(BaseModel): 
+    id : int
+    company_id : int
+    name : str 
+    email: EmailStr
+    role : str 
+    is_active : bool 
+
+    class Config: 
+        from_attributes =  True
