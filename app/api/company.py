f05d9580c4a7cd8645350a9cd7de5d196b27d7f2
Firdawsbenahmed <firdawsbenahmed20@gmail.com>
2026-05-03 14:33:44 +0100

    getting companies

--- app/api/company.py
+++ app/api/company.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session 
 from app.core.database import get_db
 from app.models.company import Company
-from app.schemas.company import CompanyCreate, CompanyResponse
+from app.schemas.company import  CompanyResponse
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
 from typing import List
@@ -12,42 +12,15 @@ from app.models.user import User
 router = APIRouter(prefix="/companies", tags=["companies"])
 
 
-@router.post("/", response_model= CompanyResponse)
-def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
-    existing = db.query(Company).filter(Company.email == company.email).first()
-    if existing : 
-        raise HTTPException(status_code=400, detail = "email already used")
-    new_company = Company(
-        name=company.name,
-        industry=company.industry,
-        email=company.email,
-        status="active"
-    )
-
-    try:
-        db.add(new_company)
-        db.commit()
-        db.refresh(new_company)
-    except IntegrityError:
-        db.rollback()
-        raise HTTPException(status_code=400, detail="Email already exists")
-
-    return new_company
-
-@router.get("/", response_model=List[CompanyResponse]) 
-def get_companies (db : Session = Depends(get_db)): 
-    companies = db.query(Company).all()
-    return companies
-
 @router.get("/me", response_model=CompanyResponse)
 def get_my_company(
-    current_user : User = Depends(get_current_user),
-    db : Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user : User = Depends (get_current_user)
 ) : 
-    company_existence = db.query(Company).filter(
-         Company.id == current_user.company_id 
+    company = db.query(Company).filter(
+        Company.id == current_user.company_id
     ).first()
-    
-    if not company_existence : 
-        raise HTTPException (status_code=404 , detail="company not found")
-    return company_existence
+    if not company : 
+        raise HTTPException(status_code=404 , detail= "company does not exist ")
+    return company 
+
