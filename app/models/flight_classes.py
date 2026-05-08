fa2069d44b705005af056854e7f7bc631b841921
Firdawsbenahmed <firdawsbenahmed20@gmail.com>
2026-05-08 14:23:01 +0100

    flight class

---
+++ app/models/flight_classes.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer,Enum, ForeignKey
+from app.core.database import Base
+
+class Flight_class(Base): 
+    __tabelname__ = "flight class"
+
+    id = Column(Integer, primary_key=True, index=True)
+    flight_id = Column(Integer, ForeignKey("flights.id"),nullable=False)
+    flight_class = Column(Enum("economy","buisness","first", name="flight_class_Enum"), nullable=False)
+    price = Column(Integer, nullable=False)
+    total_seats = Column(Integer, nullable=False)
+    available_seats = Column(Integer, nullable=False)
