25fee6f20e61b279de02975d937692c0c6bb48a8
Firdawsbenahmed <firdawsbenahmed20@gmail.com>
2026-07-13 15:34:24 +0100

    last main

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from app.api.company import router as company_router
 from app.api.product import router as product_router
 from app.api.store import router as store_router
 from app.api.inventory import router as inventory_router
-from app.api.auth import router as auth_router 
+from app.api.auth import router as auth_router
 from app.api.flight import router as flight_router
 from app.api.booking import router as booking_router
+from app.api.admin import router as admin_router
+from app.api.account import router as account_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
-app.include_router(auth_router)
 app.include_router(flight_router)
 app.include_router(booking_router)
+app.include_router(admin_router)
+app.include_router(account_router)
+app.include_router(company_router)
+app.include_router(product_router)
+app.include_router(store_router)
+app.include_router(inventory_router)
 
 from fastapi.middleware.cors import CORSMiddleware
 
